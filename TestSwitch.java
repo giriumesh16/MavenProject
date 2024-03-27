9e3f4c7ebc1c7b5e604811d6ce75cc5e7c16eaff
giriumesh16 <giri.umesh16@hotmail.com>
2024-03-27 13:20:49 +0530

    new funcatinality added

---
+++ TestSwitch.java
@@ -0,0 +1,146 @@
+/*
+import java.util.Scanner;
+public class TestSwitch {
+ public static void main(String[] args){
+  int a =10,b=20,obj;
+  System.out.println("Enter your choice : 1 2 3 4");
+  Scanner r= new Scanner(System.in);
+  obj=r.nextInt();
+
+ switch(obj)
+{
+ case 1:System.out.println("Additon "+ (a+b));
+ break;
+ case 2:System.out.println("subraction "+ (a-b));
+ break;
+ case 3:System.out.println("Multiply "+ (a*b));
+ break;
+ case 4:System.out.println("Divide " + (a/b));
+ break;
+ default:System.out.println("Invalide choic");
+  }
+ }
+}
+*/
+
+/*
+public class TestSwitch {
+ public static void main(String[] args){
+int a=1;
+while(a <=5)
+{
+ 
+System.out.println("learn Java sir");
+++a;
+
+}
+
+}
+
+}
+*/
+
+/*
+public class TestSwitch {
+ public static void main(String[] args){
+int n=1;
+
+do
+{
+
+  System.out.println(n +"check calo");
+  ++n;
+  }
+while(n<=10);
+ }
+}
+*/
+
+/*
+public class TestSwitch 
+{
+public static void main(String[] args)
+{
+ 
+ for(int a=10;a>=1;a--)
+ {
+  System.out.println(a);
+ }
+}
+}
+*/
+
+//for each loop
+
+/*
+public class TestSwitch
+{
+ public static void main(String[] args){
+  
+//int a[]={1,2,3,4,5};
+ //String a[]={"UMESH","DEEPAK","SEEMA","AARUSH","SWARA"};
+char a[]={'A','B','C','D','X','a'};
+for(char b:a)
+{
+
+System.out.println(b );
+
+}
+
+}
+}
+
+*/
+
+
+//nested for loop
+/*
+public class TestSwitch {
+ public static void main(String[] args) {
+ int i,j;
+ for(i=1;i<=2;i++)
+{
+      for(j=1;j<=2;j++)
+            {
+               System.out.print("*");
+            }
+ System.out.println();
+}
+
+}
+ 
+}
+*/
+
+
+public class TestSwitch {
+ public static void main(String[] args) {
+ int i,j;
+ for(i=1;i<=5;i++)
+{
+      for(j=5;j>=i;j--)
+            {
+               System.out.print("*");
+            }
+ System.out.println();
+}
+
+}
+ 
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
